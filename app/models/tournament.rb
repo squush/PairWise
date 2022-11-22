42cfdaf9195cd660d6faf74c5483e7809b2d369c
Andrew Wever <andrew.wever@gmail.com>
2022-11-22 13:25:58 -0500

    Setting a pairing enum for tournaments

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -3,4 +3,9 @@ class Tournament < ApplicationRecord
   belongs_to :event, optional: true
   has_many :players, dependent: :destroy
   has_many :matchups, through: :players, dependent: :destroy
+
+  enum status: {
+    swiss: 0,
+    round_robin: 1
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_172235) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_164020) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_172235) do
     t.integer "number_of_winners"
     t.integer "pairing_system"
     t.bigint "user_id", null: false
-    t.bigint "event_id"
+    t.bigint "event_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_tournaments_on_event_id"
