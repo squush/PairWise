967e59690c18fcb9f0ed75ddeed1726e2b685305
Jackson Smylie <jacksonsmylie@gmail.com>
2022-11-22 17:32:49 -0500

    made a basic scraper

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :tournaments, dependent: :destroy
+  # TODO: Figure out how to get dependent: :destroy working with destroy_all in
+  #       the seed. Currently it causes an error because (I think) the matchups
+  #       have two foreign keys to the players table. Probably need a ticket.
+  has_many :tournaments#, dependent: :destroy
   # TODO: May also want to associate players and matchups for getting historical
   #       stats for past tournaments. But that's not needed for the basic app.
 end
--- scrape.rb
+++ scrape.rb
@@ -4,4 +4,26 @@ require 'open-uri'
 url = 'https://www.cross-tables.com'
 html = URI.open(url)
 doc = Nokogiri::HTML(html)
-return doc
+tournaments = doc.css('#utblock .xtdatatable tr')
+
+tournaments.first(2).each_with_index do |tournament, index|
+  if index.positive?
+    # naspa = tournament.css('span').children.first.attribute('href').value
+    naspa = tournament.css('img').attribute('src').value == 'i/naspa.png'
+    url = "https://www.cross-tables.com#{tournament.css('span').children.first.attribute('href').value}"
+    # pp tournament
+    city = tournament.css('a').children.text
+    # pp city
+    # tournament.css('span').children.first.attribute('href').value
+    date = tournament.css('td').children.text
+    number_of_players = 0
+    # pp tournament
+    pp date
+
+  end
+end
+
+#pp tournaments[1].css('span').children.first.attribute('href').value
+
+
+# want: naspa?, city, date, # of players, href
