02376ef503fea4e8a9ea57c585ef894af5e14e6b
Andrew Wever <andrew.wever@gmail.com>
2022-11-22 16:48:56 -0500

    basic seed

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -5,6 +5,7 @@ class Tournament < ApplicationRecord
   has_many :matchups, through: :players, dependent: :destroy
 
   validates :rounds, presence: true, numericality: { greater_than: 0 }
+  # TODO: Possibly need a validation for number_of_winners, but low priority
 
   enum pairing_system: {
     swiss: 10,
