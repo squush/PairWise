7edae71c136ed3e8430f52c119ea8e1deb3d5572
Andrew Wever <andrew.wever@gmail.com>
2022-11-22 13:27:27 -0500

    Setting a pairing enum for tournaments

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   # TODO: Strictly speaking, this doesn't need an association. Long-term, maybe
   #       it makes sense to lock an event from having more than one tournament
-  #       created for it, but that would likely need some type of official
-  #       validation with NASPA
+  #       created using a has_one association, but that would likely need some
+  #       type of official validation with NASPA.
 end
--- db/migrate/20221122172235_allow_null_event_in_tournaments.rb
+++
@@ -1,5 +0,0 @@
-class AllowNullEventInTournaments < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :tournaments, :event_id, true
-  end
-end
