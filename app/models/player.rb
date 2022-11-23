a2cd6ce7469d3538a7c164133792a0d62c3c88f6
Andrew Wever <andrew.wever@gmail.com>
2022-11-23 15:12:25 -0500

    Rename association to be a bit more clear

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,8 +2,8 @@ class Player < ApplicationRecord
   belongs_to :tournament
   # The foreign_key in the lines below ensures the destroy works. Otherwise it's
   # looking for just "player_id" by default.
-  has_many :player1_matchups, class_name: "Matchup", dependent: :destroy, foreign_key: :player1_id
-  has_many :player2_matchups, class_name: "Matchup", dependent: :destroy, foreign_key: :player2_id
+  has_many :matchups_as_player1, class_name: "Matchup", dependent: :destroy, foreign_key: :player1_id
+  has_many :matchups_as_player2, class_name: "Matchup", dependent: :destroy, foreign_key: :player2_id
 
   # TODO: Validation for seed?
   validates :name, :rating, :division, presence: true
