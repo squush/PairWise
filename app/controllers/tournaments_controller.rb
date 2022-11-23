24ef36cc32622c07fcbe89c4c3880a08a5ca5cce
Andrew Wever <andrew.wever@gmail.com>
2022-11-23 12:16:50 -0500

    getting started on the tournament show route

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -2,13 +2,17 @@ require 'nokogiri'
 require 'open-uri'
 
 class TournamentsController < ApplicationController
+  before_action :set_tournament, only: %i[show]
 
-  # Starting with scraping cross-tables to build an index page for Events
   def index
     @doc = all_crosstables_events
     @events = Event.all
     # authorize @events
     @tournaments = policy_scope(Tournament)
+  end
+
+  def show
+    @tournament = Tournament.find(params[:id])
   end
 
   private
@@ -17,5 +21,10 @@ class TournamentsController < ApplicationController
     url = 'https://www.cross-tables.com'
     html = URI.open(url)
     doc = Nokogiri::HTML(html)
+  end
+
+  def set_tournament
+    @tournament = Tournament.find(params[:id])
+    authorize(@tournament)
   end
 end
