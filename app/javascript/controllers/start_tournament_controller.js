3caccca0f7579253d1197bc1586a8616f1d4d449
Jackson Smylie <jacksonsmylie@gmail.com>
2022-11-26 10:08:19 -0600

    took out unnecessary code

--- app/javascript/controllers/start_tournament_controller.js
+++ app/javascript/controllers/start_tournament_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="start-tournament"
 export default class extends Controller {
-
   static targets = [ "form", "modal", "close" ];
 
   connect() {
@@ -10,33 +9,10 @@ export default class extends Controller {
   }
 
   showForm() {
-    // console.log(event);
-    // this.formTarget.classList.toggle("d-none");
-    // this.modalTarget.classList.toggle("none")
-    // console.log(this.modalTarget);
-    // console.log(this.formTarget);
-    // console.log("hello")
     this.modalTarget.style.display = "block";
-
-    // let modalBtn = document.getElementById("modal-btn");
-    // let modal = document.querySelector(".modal");
-    // let closeBtn = document.querySelector(".close-btn");
-    // modalBtn.onclick = function(){
-    //   modal.style.display = "block";
-    // }
-    // closeBtn.onclick = function(){
-    //   modal.style.display = "none";
-    // }
-    // window.onclick = function(e){
-    //   if(e.target == modal){
-    //     modal.style.display = "none";
-    //   }
-    // }
   }
 
   closeForm() {
     this.modalTarget.style.display = "none";
   }
-
-
 }
