d9264bb5f313dd2a8b3d082306b532c4bddcddf9
Jackson Smylie <jacksonsmylie@gmail.com>
2022-11-26 10:07:35 -0600

    modal works as a popup

--- app/javascript/controllers/start_tournament_controller.js
+++ app/javascript/controllers/start_tournament_controller.js
@@ -3,14 +3,40 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="start-tournament"
 export default class extends Controller {
 
-  static targets = [ "form" ];
+  static targets = [ "form", "modal", "close" ];
 
   connect() {
     console.log("connecteddd");
   }
 
-  toggleForm(event) {
-    console.log(event);
-    this.formTarget.classList.toggle("d-none");
+  showForm() {
+    // console.log(event);
+    // this.formTarget.classList.toggle("d-none");
+    // this.modalTarget.classList.toggle("none")
+    // console.log(this.modalTarget);
+    // console.log(this.formTarget);
+    // console.log("hello")
+    this.modalTarget.style.display = "block";
+
+    // let modalBtn = document.getElementById("modal-btn");
+    // let modal = document.querySelector(".modal");
+    // let closeBtn = document.querySelector(".close-btn");
+    // modalBtn.onclick = function(){
+    //   modal.style.display = "block";
+    // }
+    // closeBtn.onclick = function(){
+    //   modal.style.display = "none";
+    // }
+    // window.onclick = function(e){
+    //   if(e.target == modal){
+    //     modal.style.display = "none";
+    //   }
+    // }
   }
+
+  closeForm() {
+    this.modalTarget.style.display = "none";
+  }
+
+
 }
