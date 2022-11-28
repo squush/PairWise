6028b8e0313bdf39ae45aaec4439970262be87ff
Jackson Smylie <jacksonsmylie@gmail.com>
2022-11-27 17:57:33 -1000

    submit score is now a modal

---
+++ app/javascript/controllers/matchups_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="matchups"
+export default class extends Controller {
+  static targets = [ "modal" ];
+
+  connect() {
+    console.log("connected");
+  }
+
+  showForm() {
+    this.modalTarget.style.display = "block";
+  }
+
+  closeForm() {
+    this.modalTarget.style.display = "none";
+  }
+}
