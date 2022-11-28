d9cba1c93915fc4ddf38c87f7266e54da82f6416
Jackson Smylie <jacksonsmylie@gmail.com>
2022-11-28 21:32:22 -0100

    Display rankings properly on scoreboard

--- db/schema.rb
+++ db/schema.rb
@@ -104,9 +104,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_204914) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.string "username"
     t.integer "crosstables_id"
-    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
