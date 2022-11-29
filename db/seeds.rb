7763d4d87ae55b0d5dd180a85507581f58cae95a
Andrew Wever <andrew.wever@gmail.com>
2022-11-29 13:02:48 -0500

    simpler seed, and use placeholder pic on matchup page if pic is missing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,34 @@
 require "open-uri"
 
-emails = ["a@a.a", "b@b.b", "c@c.c", "d@d.d", "e@e.e"]
+emails = ["a@a.a", "b@b.b"]
 
 # This just destroy the specific test users, not the whole DB
 print "Destroying test users: "
-emails.each do |email|
-  print "... #{email}"
-  User.find_by(email: email).try(:destroy)
-end
+User.find_by(email: emails[0]).try(:destroy)
+print "... #{emails[0]}"
+User.find_by(email: emails[1]).try(:destroy)
+print "... #{emails[1]}"
 puts ""
 
 # This just destroy the specific test events, not the whole DB
 puts "Destroying test events"
-Event.find_by(location: "Mile End, Montreal",  date: "2022-12-04", rounds: 3).try(:destroy)
-Event.find_by(location: "Hochelaga, Montreal", date: "2022-12-05", rounds: 2).try(:destroy)
-Event.find_by(location: "Ottawa",              date: "2022-12-04", rounds: 2).try(:destroy)
-Event.find_by(location: "Mile End, Montreal",  date: "2023-05-01", rounds: 2).try(:destroy)
-Event.find_by(location: "Laval",               date: "2022-12-12", rounds: 2).try(:destroy)
-Event.find_by(location: "Cornwall, Ontario",   date: "2022-12-11", rounds: 2).try(:destroy)
+Event.find_by(location: "Mile End, Montreal", date: "2022-12-04").try(:destroy)
+Event.find_by(location: "Laval",              date: "2022-12-07").try(:destroy)
+Event.find_by(location: "Ottawa",             date: "2022-12-10").try(:destroy)
 
 puts "Creating users"
 user_a = User.create!(email: emails[0], password: "123456")
 user_b = User.create!(email: emails[1], password: "123456")
-user_c = User.create!(email: emails[2], password: "123456")
 
 puts "Creating events"
-event_1 = Event.create!(location: "Mile End, Montreal",  date: "2022-12-04", rounds: 3)
-event_2 = Event.create!(location: "Hochelaga, Montreal", date: "2022-12-05", rounds: 2)
-event_3 = Event.create!(location: "Ottawa",              date: "2022-12-04", rounds: 2)
-event_4 = Event.create!(location: "Mile End, Montreal",  date: "2023-05-01", rounds: 2)
-event_5 = Event.create!(location: "Laval",               date: "2022-12-12", rounds: 2)
-event_6 = Event.create!(location: "Cornwall, Ontario",   date: "2022-12-11", rounds: 2)
+event_1 = Event.create!(location: "Mile End, Montreal", date: "2022-12-04", rounds: 3)
+event_2 = Event.create!(location: "Laval",              date: "2022-12-07", rounds: 3)
+event_3 = Event.create!(location: "Ottawa",             date: "2022-12-10", rounds: 5)
 
 puts "Creating tournaments"
 tourney_1 = Tournament.create!(user: user_a, event: event_1)
-tourney_2 = Tournament.create!(user: user_a, event: event_2)
-tourney_3 = Tournament.create!(user: user_a, event: event_2)
-tourney_4 = Tournament.create!(user: user_a, event: event_2, pairing_system: 20)
-tourney_5 = Tournament.create!(user: user_a, event: event_3, pairing_system: 20)
-tourney_6 = Tournament.create!(user: user_b, event: event_3)
-tourney_7 = Tournament.create!(user: user_c, event: event_3)
+tourney_2 = Tournament.create!(user: user_a, event: event_2, pairing_system: 20)
+tourney_3 = Tournament.create!(user: user_b, event: event_3)
 
 print "Creating players for tournament 1... "
 print "Jackson! "
@@ -47,85 +36,60 @@ player_1a = Player.create!(
   name: "Jackson Smylie", rating: 2006, division: 1, crosstables_id: 20032, tournament: tourney_1
 )
 
-jackson_photo = URI.open("https://www.cross-tables.com/pix/IMG_1335.jpg")
-player_1a.photo.attach(io: jackson_photo, filename: "jack.jpg", content_type: "image/jpg")
+player_1a_pic = URI.open("https://www.cross-tables.com/pix/IMG_1335.jpg")
+player_1a.photo.attach(io: player_1a_pic, filename: "jack1.jpg", content_type: "image/jpg")
 player_1a.save
 
-# player_1a.photo.attach(io: File.open('app/assets/images/jack.jpg'), filename: 'jack.jpg', content_type: 'image/jpg')
 puts "Plus some scrubs."
-player_1b = Player.create!(name: "Player B", rating: 1970, division: 1, tournament: tourney_1)
-player_1c = Player.create!(name: "Player C", rating: 1100, division: 2, tournament: tourney_1)
-player_1d = Player.create!(name: "Player D", rating: 1250, division: 2, tournament: tourney_1)
+player_1b = Player.create!(name: "Michael Early", rating: 1759, division: 1, tournament: tourney_1, crosstables_id: 437)
+player_1c = Player.create!(name: "David Nwabor", rating: 1566, division: 1, tournament: tourney_1, crosstables_id: 24056)
+player_1d = Player.create!(name: "April McCarley", rating: 1184, division: 1, tournament: tourney_1, crosstables_id: 463)
+player_1b_pic = URI.open("https://www.scrabbleplayers.org/players/e/early_michael.jpg")
+player_1b.photo.attach(io: player_1b_pic, filename: "1b.jpg", content_type: "image/jpg")
+player_1b.save
+player_1c_pic = URI.open("https://www.scrabbleplayers.org/players/n/nwabor_david_AA005783.jpg")
+player_1c.photo.attach(io: player_1c_pic, filename: "1c.jpg", content_type: "image/jpg")
+player_1c.save
+player_1d_pic = URI.open("https://www.scrabbleplayers.org/players/m/mccarley_april.jpg")
+player_1d.photo.attach(io: player_1d_pic, filename: "1d.jpg", content_type: "image/jpg")
+player_1d.save
 
 # These matchups don't follow Swiss pairing. This is just to help test the view
-puts "Creating matchups for tournament 1 (no rounds done)"
+puts "Creating matchups for tournament 1"
 Matchup.create!(round_number: 1, player1: player_1a, player2: player_1b)
 Matchup.create!(round_number: 1, player1: player_1c, player2: player_1d)
-Matchup.create!(round_number: 2, player1: player_1a, player2: player_1c)
 Matchup.create!(round_number: 2, player1: player_1b, player2: player_1d)
-Matchup.create!(round_number: 3, player1: player_1a, player2: player_1d)
-Matchup.create!(round_number: 3, player1: player_1b, player2: player_1c)
+Matchup.create!(round_number: 2, player1: player_1c, player2: player_1a)
 
-puts "Creating players for tournament 2"
-player_2a = Player.create!(name: "Player E", rating: 1800, division: 1, tournament: tourney_2)
-player_2b = Player.create!(name: "Player F", rating: 1700, division: 1, tournament: tourney_2)
-player_2c = Player.create!(name: "Player G", rating: 1700, division: 1, tournament: tourney_2)
-player_2d = Player.create!(name: "Player H", rating: 1700, division: 1, tournament: tourney_2)
-
-puts "Creating matchups for tournament 2 (partial round done)"
-matchup7 = Matchup.create!(
-  round_number: 1, done: true,
-  player1: player_2a, player1_score: 300,
-  player2: player_2b, player2_score: 250
+print "Creating players for tournament 2... "
+print "Jackson! "
+player_2a = Player.create!(
+  name: "Jackson Smylie", rating: 2006, division: 1, tournament: tourney_2, crosstables_id: 20032
 )
+
+player_2a_pic = URI.open("https://www.cross-tables.com/pix/IMG_1335.jpg")
+player_2a.photo.attach(io: player_2a_pic, filename: "jack2.jpg", content_type: "image/jpg")
+player_2a.save
+
+puts "Plus some scrubs."
+player_2b = Player.create!(name: "Michael Early", rating: 1759, division: 1, tournament: tourney_2, crosstables_id: 437)
+player_2c = Player.create!(name: "David Nwabor", rating: 1566, division: 1, tournament: tourney_2, crosstables_id: 24056)
+player_2d = Player.create!(name: "April McCarley", rating: 1184, division: 1, tournament: tourney_2, crosstables_id: 463)
+player_2b_pic = URI.open("https://www.scrabbleplayers.org/players/e/early_michael.jpg")
+player_2b.photo.attach(io: player_2b_pic, filename: "2b.jpg", content_type: "image/jpg")
+player_2b.save
+player_2c_pic = URI.open("https://www.scrabbleplayers.org/players/n/nwabor_david_AA005783.jpg")
+player_2c.photo.attach(io: player_2c_pic, filename: "2c.jpg", content_type: "image/jpg")
+player_2c.save
+player_2d_pic = URI.open("https://www.scrabbleplayers.org/players/m/mccarley_april.jpg")
+player_2d.photo.attach(io: player_2d_pic, filename: "2d.jpg", content_type: "image/jpg")
+player_2d.save
+
+# These matchups don't follow Swiss pairing. This is just to help test the view
+puts "Creating matchups for tournament 2"
+Matchup.create!(round_number: 1, player1: player_2a, player2: player_2b)
 Matchup.create!(round_number: 1, player1: player_2c, player2: player_2d)
-Matchup.create!(round_number: 2, player1: player_2a, player2: player_2c)
 Matchup.create!(round_number: 2, player1: player_2b, player2: player_2d)
+Matchup.create!(round_number: 2, player1: player_2c, player2: player_2a)
 
-puts "Creating players for tournament 3"
-player_3a = Player.create!(name: "Player I", rating: 1800, division: 1, tournament: tourney_3)
-player_3b = Player.create!(name: "Player J", rating: 1700, division: 1, tournament: tourney_3)
-player_3c = Player.create!(name: "Player K", rating: 1500, division: 2, tournament: tourney_3)
-player_3d = Player.create!(name: "Player L", rating: 1400, division: 2, tournament: tourney_3)
-
-puts "Creating matchups for tournament 3 (one round done)"
-Matchup.create!(
-  round_number: 1, done: true,
-  player1: player_3a, player1_score: 300,
-  player2: player_3b, player2_score: 250
-)
-Matchup.create!(
-  round_number: 1, done: true,
-  player1: player_3c, player1_score: 250,
-  player2: player_3d, player2_score: 275
-)
-Matchup.create!(round_number: 2, player1: player_3b, player2: player_3a)
-Matchup.create!(round_number: 2, player1: player_3d, player2: player_3c)
-
-puts "Creating players for tournament 4 (full tournament done)"
-player_4a = Player.create!(name: "Player M", rating: 1800, division: 1, tournament: tourney_4)
-player_4b = Player.create!(name: "Player N", rating: 1700, division: 1, tournament: tourney_4)
-player_4c = Player.create!(name: "Player O", rating: 1500, division: 2, tournament: tourney_4)
-player_4d = Player.create!(name: "Player P", rating: 1400, division: 2, tournament: tourney_4)
-
-puts "Creating matchups for tournament 4"
-Matchup.create!(
-  round_number: 1, done: true,
-  player1: player_4a, player1_score: 375,
-  player2: player_4b, player2_score: 325
-)
-Matchup.create!(
-  round_number: 1, done: true,
-  player1: player_4c, player1_score: 300,
-  player2: player_4d, player2_score: 250
-)
-Matchup.create!(
-  round_number: 2, done: true,
-  player1: player_4b, player1_score: 400,
-  player2: player_4a, player2_score: 390
-)
-Matchup.create!(
-  round_number: 2, done: true,
-  player1: player_4d, player1_score: 300,
-  player2: player_4c, player2_score: 280
-)
+puts ""
