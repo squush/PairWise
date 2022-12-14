b2498cbc8b0b5d25637a6ee2e53268bfdf377dff
Andrew Wever <andrew.wever@gmail.com>
2022-12-14 17:35:14 -0500

    add TODO comment about a bug

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -5,6 +5,8 @@ class Tournament < ApplicationRecord
   has_many :matchups, through: :players, source: :matchups_as_player1
 
   validates :number_of_winners, presence: true
+  # TODO: There's still a bug if the user manually clears this field in the form
+  #       when creating a new tournament
 
   enum pairing_system: {
     "Swiss" => 10,
