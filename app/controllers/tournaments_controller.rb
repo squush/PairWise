686c16a4b7584c877c59e6b60e07305849b993d4
Martin DeMello <martindemello@gmail.com>
2022-12-14 19:36:29 -0800

    Factor out the crosstables fetcher into its own lib

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,3 +1,5 @@
+require 'crosstables_fetcher'
+
 class TournamentsController < ApplicationController
   before_action :set_tournament, only: %i[show edit update destroy]
   skip_before_action :authenticate_user!, only: %i[index show scoreboard]
@@ -133,126 +135,26 @@ class TournamentsController < ApplicationController
   end
 
   def get_players(tournament)
-    url = "https://www.cross-tables.com/entrants.php?u=#{tournament.event.xtables_id}"
-    html = URI.open(url)
-    doc = Nokogiri::HTML(html)
-
-    data = doc.css('tr.headerrow ~ tr')
-    division = 1
-    data.each do |child|
-      row = child.search('td').text
-      if row.start_with?("Division")
-        division = row[/\d/].to_i
-      elsif row.start_with?(/\d/)
-        seed = row[/\d+/].to_i
-        name = child.search('td')[1].text.gsub('*', '').strip[1..-1]
-        rating = child.search('td')[2].text.strip
-        xtables_id = child.search('td').children.children.css('a').attribute('href').value[/\d{1,5}/]
-        if rating == "---"
-          rating = 0
-        else
-          rating = rating.to_i
-        end
-
-        player = Player.create!(
-          name: name,
-          rating: rating,
-          new_rating: rating,
-          division: division,
-          seed: seed,
-          ranking: seed,
-          crosstables_id: xtables_id,
-          tournament: tournament
-        )
-
-        # The path by default can be either a full path to a photo, for example
-        # on scrabbleplayers.org, or a relative path if it's stored directly on
-        # cross-tables.
-        photo_path = get_player_photo(xtables_id)
-
-        # The gsub is needed because pics with a space in the filename prevent
-        # the URI from being opened.
-        # TODO: There might be other characters that will break this. Instead of
-        #       gsubbing one by one, there might be a cleaner way to fix this.
-        photo_path.gsub!(" ", "%20")
-
-        # When the path is relative, we need to create a full URL from it
-        if photo_path.start_with?("/")
-          photo_path = "https://www.cross-tables.com#{photo_path}"
-        end
-
-        user_photo = URI.open(photo_path)
-        player.photo.attach(io: user_photo, filename: "player_pic.jpg", content_type: "image/jpg")
-        player.save
-      end
+    players = CrosstablesFetcher.get_players(tournament.event.xtables_id)
+    for p in players do
+      player = Player.create!(**p)
+      # The path by default can be either a full path to a photo, for example
+      # on scrabbleplayers.org, or a relative path if it's stored directly on
+      # cross-tables.
+      user_photo = CrosstablesFetcher.get_player_photo(player.crosstables_id)
+      player.photo.attach(io: user_photo, filename: "player_pic.jpg", content_type: "image/jpg")
+      player.save
     end
-  end
-
-  # Grabs the source path of the player's pic based on the player's xtables ID
-  def get_player_photo(xtables_id)
-    url = "https://www.cross-tables.com/results.php?p=#{xtables_id}"
-    html = URI.open(url)
-    doc = Nokogiri::HTML(html)
-    photo = doc.css('img.playerpic')[0][:src]
   end
 
   def all_crosstables_events
-    url = 'https://www.cross-tables.com'
-    html = URI.open(url)
-    doc = Nokogiri::HTML(html)
-    # Find all tags that use the "rowupcoming#" id. This is the CSS id used for
-    # each row in the upcoming events table.
-    tournaments = doc.xpath('//tr[starts-with(@id, "rowupcoming")]')
-
-    tournaments.each_with_index do |tournament, index|
-      # Check if it's a NASPA event
-      naspa = tournament.css('img').attribute('src').value == 'i/naspa.png'
-
-      # If it's a NASPA event, get its date and location, and create a new
-      # record for that event
-      if naspa
-        @location = tournament.css('a').children.text
-
-        # May want to use this date method below to get the tournament's start
-        # date so that we can sort upcoming events by start date
-        # TODO: Fix the month_and_day regex to grab date ranges. Currently,
-        #       multi-day events are setting each event on the first day. Check
-        #       Kingston, as an example
-        month_and_day = tournament.css('td')[-2].children.text[/^\d*\/\d*/]
-        year = tournament.css('td')[-2].children.text[/\d{4}/] || Date.today.year
-        @sortable_date = Date.parse("#{year}/#{month_and_day}")
-
-        tournament.css('span').children.each do |event|
-          create_event(event)
-        end
+    events = CrosstablesFetcher.get_all_naspa_events
+    for event in events do
+      # TODO: This hits the db once per event
+      unless Event.find_by(xtables_id: event[:xtables_id])
+        Event.create!(**event)
       end
     end
-  end
-
-  def create_event(event)
-    url = "https://www.cross-tables.com#{event.attribute('href').value}"
-    html = URI.open(url)
-    doc = Nokogiri::HTML(html)
-
-    # This returns a string date, since often the date is a range,
-    # which is not easy to parse into a Date object. See above
-    date = doc.css('p').children[2].text[/\w.*20\d\d/]
-    xtables_id = event.attribute('href').value[/\d+$/].to_i
-    number_of_players = doc.css('p').children[8].text.to_i
-    rounds = doc.css('td').children.text[/games:.\d*/][/\d+/]
-    doc.css('td').children.each do |line|
-      if line.text[/^Division \d/]
-        divisions = line.text[/ [123456789] /].chomp.to_i
-      end
-    end
-
-    Event.create!(
-      location: @location,
-      rounds: rounds,
-      number_of_players: number_of_players,
-      date: @sortable_date,
-      xtables_id: xtables_id,
-      divisions: divisions) unless Event.find_by(xtables_id: xtables_id)
   end
 
   def generate_two_rounds_matchups(tournament)
