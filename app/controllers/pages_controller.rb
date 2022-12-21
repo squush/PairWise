1a486fe596ae04a11983bea1c321cdda76a0e8f0
Jackson Smylie <jacksonsmylie@gmail.com>
2022-12-20 23:30:20 -0500

    added user photos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,19 @@
+require_relative '../lib/crosstables_fetcher'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if user_signed_in? && !current_user.photo.attached?
+      if current_user.crosstables_id
+        user_photo = CrosstablesFetcher.get_player_photo(current_user.crosstables_id)
+        current_user.photo.attach(io: user_photo, filename: "user_pic.jpg", content_type: "image/jpg")
+        current_user.save
+      else
+        current_user.photo.attach(io: File.open('app/assets/images/user-astronaut-solid.svg'), filename: "user-astronaut-solid.svg", content_type: "image/svg+xml")
+        current_user.save
+      end
+    end
   end
 
   # TODO: Remove if we don't add this feature
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tournaments, dependent: :destroy
+  has_one_attached :photo
+  validates :crosstables_id, uniqueness: true, allow_blank: true, allow_nil: true
   # TODO: May also want to associate players and matchups for getting historical
   #       stats for past tournaments. But that's not needed for the basic app.
 end
