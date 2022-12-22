e19ead11c841e0730633d30201b5aed68754275d
Martin DeMello <martindemello@gmail.com>
2022-12-21 17:29:23 -0800

    move stuff out of the player controller

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -15,6 +15,15 @@ class Tournament < ApplicationRecord
   #   "KOTH" => 30
   # }
 
+  def current_round
+    p = players.first
+    p.win_count.to_i + p.loss_count.to_i + 1
+  end
+
+  def division_size(division)
+    players.for_division(division).non_bye.count
+  end
+
   def rounds_to_display
     # Rounds with matchups to display
     (1..self.event.rounds).select do |round|
@@ -24,4 +33,31 @@ class Tournament < ApplicationRecord
         round_matchups.inactive.count > 0
     end
   end
+
+  def create_or_retrieve_bye!(division)
+    Player.find_by(tournament: self, name: "Bye", division: division) ||
+      Player.create!(name: "Bye", tournament: self, rating: 0, new_rating: 0,
+                     division: division, win_count: 0, seed: 0)
+  end
+
+  def add_new_player(player)
+    # Add a new player to this tournament, adding a bye as well if needed.
+    div = player.division
+    round = current_round
+    if division_size(div).odd?
+      bye = create_or_retrieve_bye!(div)
+      Matchup.create!(round_number: round, player1: player, player2: bye)
+      Matchup.create!(round_number: round + 1, player1: player, player2: bye)
+    else
+      # Find any current bye matches, replace the bye with the new player.
+      bye_matchups = matchups.select { |matchup|
+        matchup.player2.name == "Bye" && matchup.round_number >= round
+      }
+      bye_matchups.each do |matchup|
+        matchup.player2 = player
+        matchup.save
+      end
+    end
+  end
+
 end
