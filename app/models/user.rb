f609146a1a3df1be68b2bb508d728a8e8926bc19
Jackson Smylie <jacksonsmylie@gmail.com>
2022-12-23 16:21:28 -0500

    basic changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,13 @@ class User < ApplicationRecord
   validates :crosstables_id, uniqueness: true, allow_blank: true, allow_nil: true
   # TODO: May also want to associate players and matchups for getting historical
   #       stats for past tournaments. But that's not needed for the basic app.
+
+  def tournaments_as_player
+    if self.crosstables_id
+      @player = Player.where(crosstables_id: self.crosstables_id).first
+      @tournaments = @player.tournaments
+    else
+      @tournaments = []
+    end
+  end
 end
